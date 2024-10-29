54e6c5b582cdfeda8dbc569d35b4ee935536b752
Mudather-cell <mzeinelabdeen@yahoo.com>
2024-10-29 09:46:59 +0000

    Python Trial

---
+++ Salution.py
@@ -0,0 +1,3 @@
+print("Hi my name is Mudather Zeinelabeen")
+print("Iam a teacher as well as a researcher at John Ruskin College")
+print("I have just started working at this college about a couple of months ago")
